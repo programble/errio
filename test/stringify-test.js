246209722a5dc6de63da29a4409f3c725cb2272f
Curtis McEnroe <programble@gmail.com>
2015-01-09 22:56:00 -0500

    Add stringify tests

--- test/stringify-test.js
+++ test/stringify-test.js
@@ -1,9 +1,20 @@
+var assert = require('assert');
+
 var SuperError = require('super-error');
 var Errio = require('..');
 
 var common = require('./common');
 
 describe('stringify', function() {
-  it('serializes to a JSON object');
-  it('passes option overrides');
+  it('serializes to a JSON object', function() {
+    var json = Errio.stringify(new Error('test'));
+    assert.equal(typeof json, 'string');
+    JSON.parse(json);
+  });
+
+  it('passes option overrides', function() {
+    var json = Errio.stringify(new Error('test'), { stack: true });
+    var object = JSON.parse(json);
+    assert.equal(typeof object.stack, 'string');
+  });
 });
