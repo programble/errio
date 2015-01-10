a68db9dade1917cdd7770f0d26af14ad5561aa21
Curtis McEnroe <programble@gmail.com>
2015-01-09 22:02:32 -0500

    Add registerAll tests

--- test/register-all-test.js
+++ test/register-all-test.js
@@ -4,5 +4,11 @@ var Errio = require('..');
 var common = require('./common');
 
 describe('registerAll', function() {
-  it('sets option overrides for all error classes');
+  it('sets option overrides for all error classes', function() {
+    var FirstError = SuperError.subclass('RegisterAllFirstTestError');
+    var SecondError = SuperError.subclass('RegisterAllSecondTestError');
+    Errio.registerAll([ FirstError, SecondError ], { stack: true });
+    common.toObject.testStackTrue(FirstError);
+    common.toObject.testStackTrue(SecondError);
+  });
 });
